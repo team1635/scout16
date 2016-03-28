aa362468fd214ab4f50ab9aacdb79a71d5c6d02e
Bogdan <bradubv@hotmail.com>
2016-03-28 18:08:47 -0400

    Replace generic defense names if defs are set

--- app/lib/js/index/index.js
+++ app/lib/js/index/index.js
@@ -1,3 +1,5 @@
+var curMatch;
+
 catchEvent(window,"load", function() {
   getCurrentGame();
 });
@@ -21,6 +23,7 @@ function processCurrentMatch() {
       // evaluate JSON
       eval("var match = ("+ xmlHttpObj.responseText+")");
       //document.getElementById('match__id').value = match.id;
+      curMatch = match;
       match__id = match.id;
       document.getElementById('number_').value = match.number_;
       document.getElementById('event_name').innerHTML = match.event_name;
@@ -57,32 +60,38 @@ function blue1_btn() {
   //TODO: Clean spaces from the button string
   document.getElementById('team_id').value = 
     document.getElementById('blue1_btn').innerHTML;
+  setDefenses("Blue");
 }
 
 function blue2_btn() {
   document.getElementById('team_id').value = 
     document.getElementById('blue2_btn').innerHTML;
+  setDefenses("Blue");
 }
 
 function blue3_btn() {
   document.getElementById('team_id').value = 
     document.getElementById('blue3_btn').innerHTML;
+  setDefenses("Blue");
 }
 
 function red1_btn() {
   //TODO: Clean spaces from the button string
   document.getElementById('team_id').value = 
     document.getElementById('red1_btn').innerHTML;
+  setDefenses("Red");
 }
 
 function red2_btn() {
   document.getElementById('team_id').value = 
     document.getElementById('red2_btn').innerHTML;
+  setDefenses("Red");
 }
 
 function red3_btn() {
   document.getElementById('team_id').value = 
     document.getElementById('red3_btn').innerHTML;
+  setDefenses("Red");
 }
 
 function saveStat() {
@@ -104,4 +113,39 @@ function saveStat() {
   }
 
   request.send(params)
+}
+
+function setDefenses(allianceColor) {
+  if (allianceColor == "Red") {
+    changeButtonLabel('def2_btn', 'red_def2');
+    changeButtonLabel('def3_btn', 'red_def3');
+    changeButtonLabel('def4_btn', 'red_def4');
+    changeButtonLabel('def5_btn', 'red_def5');
+  } else {
+    changeButtonLabel('def2_btn', 'blue_def2');
+    changeButtonLabel('def3_btn', 'blue_def3');
+    changeButtonLabel('def4_btn', 'blue_def4');
+    changeButtonLabel('def5_btn', 'blue_def5');
+  }
+}
+
+function changeButtonLabel(defButton, defPropName) {
+  if (curMatch[defPropName] != null) {
+    var newLabel = curMatch[defPropName];
+    var buttonId = 'cross_' + defButton;
+    var btn = document.getElementById(buttonId);
+    var existingLabel = btn.innerHTML;
+    btn.innerHTML = existingLabel.replace(/Def\s[2-5]/, newLabel);
+    
+    var openButtonId = 'open_' + defButton;
+    var openBtn = document.getElementById(openButtonId);
+    var existingOpenLabel = openBtn.innerHTML;
+    if ((newLabel == 'A_Portcullis') || (newLabel == 'C_SallyPort')
+        || (newLabel == 'C_Drawbridge')) {
+      openBtn.disabled = false;
+      openBtn.innerHTML = existingLabel.replace(/Def\s[2-5]/, newLabel);
+    } else {
+      openBtn.disabled = true;
+    }
+  }
 }
