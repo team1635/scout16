4e5ac18bdbfa7ab1e6014757eb8a104657a8647b
Bogdan <bradubv@hotmail.com>
2016-03-29 22:41:33 -0400

    Added all team option to getTeamReport

--- app/getTeamReport.php
+++ app/getTeamReport.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <title>Scout 15: Team report</title>
-    <meta charset="utf-8">\
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content=width=device-width, initial-scale=1">
     <link rel="stylesheet" href="lib/css/bootstrap.min.css" />
@@ -10,10 +10,21 @@
   <body>
 <?php
 require_once 'infodb.php';
+require_once 'scoutUtil.php';
+
 $post_data = file_get_contents("php://input"); //debug
-isset($_GET['team_id']) or
-  die("<p>Need team number. Got $post_data</p>");
-$team_id = SanitizeString($_GET['team_id']);
+if (defined('STDIN')) {
+  $team_id = $argv[1];
+} else { 
+  if(isset($_GET['team_id'])) { 
+    $team_id = SanitizeString($_GET['team_id']);
+  }
+}
+
+$single_team = false;
+if (! empty($team_id)) {
+    $single_team = true;
+}
 
 $db_server = mysql_connect($db_hostname, $db_username , $db_password);
 if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
@@ -21,8 +32,14 @@ if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
 mysql_select_db($db_database) or
   die("Unable to select database: " . mysql_error());
 
-$query =
-"SELECT COUNT(*) as 'scouted_matches' " 
+$query = "SELECT ";
+
+if (! $single_team) {
+    $query = $query . "team_id as 'team', ";
+}
+
+$query = $query
+. "COUNT(*) as 'scouted_matches' " 
 . "  , AVG(IFNULL(auto_reach_defense,0)) as 'avg_auto_reach_defense' "
 . "  , AVG(IFNULL(auto_cross_defense,0)) as 'avg_auto_cross_defense' "
 . "  , AVG(IFNULL(auto_low_goal,0)) as 'avg_auto_low_goal' "
@@ -39,61 +56,105 @@ $query =
 . "  , AVG(IFNULL(defense,0)) as 'avg_defense' "
 . "  , AVG(IFNULL(fouls,0)) as 'avg_fouls' " 
 . "  , AVG(IFNULL(died,0)) as 'avg_deaths' " 
-. "FROM stat " 
-. "WHERE team_id = $team_id ";
+. "FROM stat ";
+
+if ($single_team) {
+  $query = $query . "WHERE team_id = $team_id ";
+} else {
+    $query = $query . "GROUP BY team_id ";
+}
 
 $result = mysql_query($query);
 //echo "<p>$query</p>"; //debug
+
 if (!$result) die("Database query failed: " . mysql_error());
 
-echo "<div class=\"panel panel-default\">Scouting report for team $team_id";
+if ($single_team) {
+    echo_single_team($result, $team_id);
+} else {
+    echo_all_teams($result);
+}
+
+function echo_single_team($sql_result, $team_number) {
+
+echo '<div class="panel panel-default">Scouting report for team ' . $team_number;
 echo '</div>';
 
 echo '<table class="table table-striped"><tbody>';
 echo "<tr><td>Scouted matches</td><td>";
-echo mysql_result($result, 0, 'scouted_matches');
+echo mysql_result($sql_result, 0, 'scouted_matches');
 echo "</td></tr><tr><td>Reach Defense in Auto</td><td>";
-echo mysql_result($result, 0, 'avg_auto_reach_defense');
+echo mysql_result($sql_result, 0, 'avg_auto_reach_defense');
 echo "</td></tr><tr><td>Cross Defense in Auto</td><td>";
-echo mysql_result($result, 0, 'avg_auto_cross_defense');
+echo mysql_result($sql_result, 0, 'avg_auto_cross_defense');
 echo "</td></tr><tr><td>Low Goal in Auto</td><td>";
-echo mysql_result($result, 0, 'avg_auto_low_goal');
+echo mysql_result($sql_result, 0, 'avg_auto_low_goal');
 echo "</td></tr><tr><td>High Goal in Auto</td><td>";
-echo mysql_result($result, 0, 'avg_auto_high_goal');
+echo mysql_result($sql_result, 0, 'avg_auto_high_goal');
 
 echo "</td></tr><tr><td>Cross Low Bar</td><td>";
-echo mysql_result($result, 0, 'avg_cross_low_bar');
+echo mysql_result($sql_result, 0, 'avg_cross_low_bar');
 
 echo "</td></tr><tr><td>Pick Ball</td><td>";
-echo mysql_result($result, 0, 'avg_pick_boulder');
+echo mysql_result($sql_result, 0, 'avg_pick_boulder');
 echo "</td></tr><tr><td>Pass Ball</td><td>";
-echo mysql_result($result, 0, 'avg_pass_boulder'); 
+echo mysql_result($sql_result, 0, 'avg_pass_boulder'); 
 echo "</td></tr><tr><td>Score Low</td><td>";
-echo mysql_result($result, 0, 'avg_score_low');
+echo mysql_result($sql_result, 0, 'avg_score_low');
 echo "</td></tr><tr><td>Score High</td><td>";
-echo mysql_result($result, 0, 'avg_score_high');
+echo mysql_result($sql_result, 0, 'avg_score_high');
 echo "</td></tr><tr><td>Get on tower platform at end</td><td>";
-echo mysql_result($result, 0, 'avg_get_on_tower');
+echo mysql_result($sql_result, 0, 'avg_get_on_tower');
 echo "</td></tr><tr><td>Climb tower</td><td>";
-echo mysql_result($result, 0, 'avg_climb_tower');
+echo mysql_result($sql_result, 0, 'avg_climb_tower');
 echo "</td></tr><tr><td>Defense moves</td><td>";
-echo mysql_result($result, 0, 'avg_defense');
+echo mysql_result($sql_result, 0, 'avg_defense');
 echo "</td></tr><tr><td>Fouls</td><td>";
-echo mysql_result($result, 0, 'avg_fouls');
+echo mysql_result($sql_result, 0, 'avg_fouls');
 echo "</td></tr><tr><td>Deaths</td><td>";
-echo mysql_result($result, 0, 'avg_deaths');
+echo mysql_result($sql_result, 0, 'avg_deaths');
 echo "</td></tr></tbody></table>";
+}
 
-function SanitizeString($var) {
-    if (empty($var)) {
-        $var = 'NULL';
-    } else {
-        $var = strip_tags($var);
-        $var = htmlentities($var);
-        $var = stripslashes($var);
-    }
+function echo_all_teams($sql_result) {
 
-    return $var;
+  echo '<table class="table table-striped">';
+  echo '<thead><tr><th>Team</th><th>Scouted Matches</th>';
+  echo '<th>Auto Reach Defense</th><th>Auto Cross Defense</th>';
+  echo '<th>Auto Low Goal</th><th>Auto High Goal</th>';
+  
+  echo '<th>Cross Low Bar</th><!--th>Cross Cheval</th-->';
+
+  echo '<th>Pick Ball</th><th>Pass Ball</th>';
+  echo '<th>Score Low</th><th>Score High</th>';
+  echo '<th>Reach Twr</th><th>Climb Twr</th>';
+  echo '<th>Defense</th><th>Fouls</th>';
+  echo '<th>Died</th>';
+
+  echo '</tr><tbody>';
+  while ($row = mysql_fetch_assoc($sql_result)) {
+    echo '<tr>';
+    echo '<td>' . $row["team"] . '</td>';
+    echo '<td>' . $row["scouted_matches"] .'</td>';
+    echo '<td>' . $row["avg_auto_reach_defense"] . '</td>';
+    echo '<td>' . $row["avg_auto_cross_defense"] . '</td>';
+    echo '<td>' . $row["avg_auto_low_goal"] . '</td>';
+    echo '<td>' . $row["avg_auto_high_goal"] .'</td>';
+    
+    echo '<td>' . $row["avg_cross_low_bar"] . '</td>';
+
+    echo '<td>' . $row["avg_pick_boulder"] . '</td>';
+    echo '<td>' . $row["avg_pass_boulder"] . '</td>';
+    echo '<td>' . $row["avg_score_low"] . '</td>';
+    echo '<td>' . $row["avg_score_high"] . '</td>';
+    echo '<td>' . $row["avg_get_on_tower"] . '</td>';
+    echo '<td>' . $row["avg_climb_tower"] . '</td>';
+    echo '<td>' . $row["avg_defense"] . '</td>';
+    echo '<td>' . $row["avg_fouls"] . '</td>';
+    echo '<td>' . $row["avg_deaths"] . '</td>';
+    echo '</tr>';
+  }
+  echo "</td></tr></tbody></table>";
 }
 
 ?>
